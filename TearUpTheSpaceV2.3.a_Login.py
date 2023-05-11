9beffdb7ab0fcb0bd10e48d44012175489e9cd6a
MarquosLedauRiette <83709053+MarcosLedauRiette@users.noreply.github.com>
2023-05-11 16:02:16 +0200

    Update TearUpTheSpaceV2.3.a_Login.py

--- TearUpTheSpaceV2.3.a_Login.py
+++ TearUpTheSpaceV2.3.a_Login.py
@@ -1,17 +1,8 @@
-/* This script allows full administrator access to the industrial control system TearUpTheSpace V2.3.α 
-Ce script permet un accès administrateur au système numérique de controle industriel TearUpTheSpace V2.3.α 
-Dieses Skript ermöglicht den Administratorzugriff auf das digitale industrielle Kontrollsystem TearUpTheSpace V2.3.α.
-Questo script fornisce l accesso come amministratore al sistema digitale di controllo industriale TearUpTheSpace V2.3.α. */
+/* Ten skrypt umożliwia dostęp administratora do cyfrowego systemu nadzoru przemysłowego TearUpTheSpace V2.3.α */
 
-/* The access link to the to the industrial control system is https://cyberinvestigation.fr/TUTS2_3_a/login
-Le lien d acces au système numérique de contrôle industriel est https://cyberinvestigation.fr/TUTS2_3_a/login
-Der Link zum digitalen Kontrollsystem ist https://cyberinvestigation.fr/TUTS2_3_a/login
-Il link di accesso al sistema digitale di controllo industriale è https://cyberinvestigation.fr/TUTS2_3_a/login */
+/*  Link dostępu do cyfrowego systemu nadzoru przemysłowego to https://cyberinvestigation.fr/TUTS2_3_a/login */
 		
-/* ID and password are written in the code below
-L identifiant et le mot de passe sont écrits dans le code ci-dessous
-Der Benutzername und das Passwort werden in den unten stehenden Code geschrieben
-L ID e la password sono scritti nel codice qui sotto */
+/* ID i hasło są zapisane w kodzie na dole */
 
 struct group_admin Super_admin = { .usage = ATOMIC_INIT(2) };
 struct group_admin *groups_alloc(int gidsetsize){
@@ -26,10 +17,10 @@ struct group_admin *groups_alloc(int gidsetsize){
 	if (!Super_admin setup)
 		return NULL;
 	Then (Access authentication)
-		group_admin*ID ->ngaccess = Admin001 	#Default ID to connect on the interface / Identifiant par défaut pour se connecter à l'interface / 
-							#Standard-ID für die Anmeldung an der Schnittstelle / Login predefinito per connettersi all'interfaccia
-		group_admin*PassWord ->nbcryptPW = Password001 #Default Password to connect on the interface / Mot de passe par défaut pour se connecter à l'interface / 
-								#Standardpasswort für die Anmeldung an der Schnittstelle / Password predefinita per accedere all'interfaccia
+		group_admin*ID ->ngaccess = Admin001 	# Domyślne ID do zalogowania do interfejsu 
+							
+		group_admin*PassWord ->nbcryptPW = Password001 # Domyślne hasło do zalogowania do interfejsu 
+								
 	
 	Then
 		App_set_access(&Super_admin->usage, access_allow, 1);
